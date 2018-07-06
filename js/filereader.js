fb810d501a62068ada600b865b05dd654a4855b1
Panda4994 <panda4994.contact@gmail.com>
2018-07-06 16:52:58 +0200

    Move conversion data to JSON-files

--- js/filereader.js
+++ js/filereader.js
@@ -1,18 +1,61 @@
-
-function readTextFile(filename) {
-    fetch("index.html").then(response => response.text()).then(text => console.log(text));
-/*    return new Promise((resolve, reject) => {
-        let file = new XMLHttpRequest();
-        file.onreadystatechange = function() {
-            if (file.readyState === 4) {
-                if (file.status === 200 || file.status === 0) {
-                    resolve(file.responseText);
-                }
-            }
-        };
-        file.open("GET", filename, true);
-        reject(file);
-    });*/
+function getJSON(filename) {
+    return $.ajax({
+        dataType: "json",
+        url: filename,
+        mimeType: "application/json",
+    }).then((obj) => {
+        return obj;
+    });
 }
 
-readTextFile();
+let default_choices = [{
+        "style":{
+            "color":"#c6993f",
+            "dashes":false
+        },
+        "name":"Crafting",
+        "value":"Crafting",
+        "description":"Crafting",
+        "type": "crafting"
+    },{
+        "style":{
+            "color":"#ed0e07",
+            "dashes":false
+        },
+        "name":"Smelting",
+        "value":"Smelting",
+        "description":"Smelting",
+        "type":"furnace"
+    }];
+
+function loadContent() {
+    getJSON("data/network.data.json")
+    .then((choices) => {
+        window.choices = choices;
+        // TODO Make sure all content is loaded before being accessed
+        fillChoices();
+        loadConversions();
+    }).fail((error) => {
+        if (error.status == 0) {
+            window.choices = default_choices;
+            fillChoices();
+            console.error("Failed to load conversion types. Hosted locally?");
+        } else {
+            console.error(error);
+        }
+    });
+}
+
+var ItemConversions = {};
+
+function loadConversions() {
+    for (let entry of window.choices) {
+        console.info("Loading conversion type \"" + entry.type + "\".");
+        getJSON("data/conversions/" + entry.type + ".json")
+        .then((content) => {
+            ItemConversions[entry.type] = content;
+        }).fail((error) => {
+            console.error("Failed to load conversion \"" + entry.type + "\". Error: ", error);
+        });
+    }
+}
